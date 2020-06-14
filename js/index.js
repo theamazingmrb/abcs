8614ce8320506feeb69d1980c477789a00d10481
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2020-06-14 13:00:18 -0700

    current working status

--- js/index.js
+++ js/index.js
@@ -6,8 +6,34 @@ let displayedLetter = document.querySelector(".letter");
 let test = document.querySelector(".test");
 let letterId = Number(card.getAttribute("letter"));
 
-console.log(`displayedLetter letter is ${displayedLetter}`);
-console.log(`${card.getAttribute("letter")}`);
+const imageNames = {
+  0: "Apple",
+  1: "Banana",
+  2: "Cup",
+  3: "Duck",
+  4: "Elephant",
+  5: "Frog",
+  6: "Gorilla",
+  7: "Horse",
+  8: "Ice Cream",
+  9: "Juice",
+  10: "Keys",
+  11: "Lion",
+  12: "Moon",
+  13: "Nose",
+  14: "Orange",
+  15: "Pig",
+  16: "Quarter",
+  17: "Rainbow",
+  18: "Sun",
+  19: "Turtle",
+  20: "Ubrella",
+  21: "Van",
+  22: "Whale",
+  23: "Xylophone",
+  24: "Yoyo",
+  25: "Zebra",
+};
 
 const updateCurrentLetter = (dir) => {
   currentLetter = letters[card.getAttribute("letter")];
@@ -15,6 +41,7 @@ const updateCurrentLetter = (dir) => {
 
 const updateCurrentImage = (id) => {
   document.getElementById("letter-image").src = `./images/${id}.jpg`;
+  document.getElementById("letter-image").alt = imageNames[id];
 };
 
 // handle left and right arrow button clicks
@@ -22,22 +49,35 @@ const updateCurrentImage = (id) => {
 const updateCard = (dir) => {
   console.log(card.getAttribute("letter"));
 
-  if (card.getAttribute("letter") < 0) {
-    card.setAttribute("letter", 25);
-    updateCurrentLetter();
-    displayedLetter.innerHTML = currentLetter;
-    console.log("in update card");
-    return;
-  }
+  const checkMinMaxLettid = () => {
+    if (card.getAttribute("letter") < 0) {
+      card.setAttribute("letter", 25);
+      letterId = 25;
+      updateCurrentLetter();
+      displayedLetter.innerHTML = currentLetter;
+      return;
+    }
+
+    if (card.getAttribute("letter") > 25) {
+      card.setAttribute("letter", 0);
+      letterId = 0;
+      updateCurrentLetter();
+      displayedLetter.innerHTML = currentLetter;
+      return;
+    }
+  };
 
   if (dir == "arrow-left") {
     card.setAttribute("letter", --letterId);
+    checkMinMaxLettid();
     updateCurrentLetter(dir);
     displayedLetter.innerHTML = currentLetter;
     updateCurrentImage(letterId);
+
     return;
   }
   card.setAttribute("letter", ++letterId);
+  checkMinMaxLettid();
   updateCurrentLetter(dir);
   displayedLetter.innerHTML = currentLetter;
   updateCurrentImage(letterId);
@@ -72,16 +112,19 @@ function populateVoiceList() {
 
     option.setAttribute("data-lang", voices[i].lang);
     option.setAttribute("data-name", voices[i].name);
-    console.log(option);
   }
 }
 
 populateVoiceList();
-var utterThis = new SpeechSynthesisUtterance("C");
 
 const speachHandler = (event) => {
-  console.log(test);
-  console.log(event.srcElement.attributes.letter);
+  let speachText;
+  if (event.target.className == "letter") {
+    speachText = event.target.innerHTML;
+  } else {
+    speachText = event.srcElement.attributes.alt.value;
+  }
+  var utterThis = new SpeechSynthesisUtterance(speachText);
 
   utterThis.pitch = 2;
   utterThis.rate = 1;
@@ -93,3 +136,5 @@ const speachHandler = (event) => {
 document
   .querySelector("#letter-image")
   .addEventListener("click", speachHandler);
+
+document.querySelector(".letter").addEventListener("click", speachHandler);
